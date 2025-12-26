b7a18aa4e4d859634248a00513138eea996cd140
[CLOUD4] 한현 <gusgus1467@naver.com>
2025-12-26 22:19:06 +0900

    feat: tailwind 관련 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,30 @@ export default {
       fontFamily: {
         sans: ['"NanumSquare"', "sans-serif"],
       },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "var(--primary)",
+          foreground: "var(--primary-foreground)",
+        },
+        secondary: {
+          DEFAULT: "var(--secondary)",
+          foreground: "var(--secondary-foreground)",
+        },
+        destructive: {
+          DEFAULT: "var(--destructive)",
+          foreground: "var(--destructive-foreground)",
+        },
+        accent: {
+          DEFAULT: "var(--accent)",
+          foreground: "var(--accent-foreground)",
+        },
+        muted: {
+          DEFAULT: "var(--muted)",
+          foreground: "var(--muted-foreground)",
+        },
+      },
     },
   },
   plugins: [],
