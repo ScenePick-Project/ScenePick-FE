8c880e50da6930ba295b8e1c9bcadec70a9258d2
songhee <songhee9327@gmail.com>
2026-01-19 22:23:27 +0900

    style: 로고 디자인 수정

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,29 +1,38 @@
 import { Link } from "react-router-dom";
 import { Button } from "../ui/Button.tsx";
-import { BiMoviePlay } from "react-icons/bi";
+import { Logo } from "@components/common/Logo.tsx";
 
 export default function Navbar() {
   return (
     <header className="fixed top-0 left-0 w-full h-16 bg-white border-b border-gray-200 z-50 flex items-center px-4">
       <div className="flex items-center justify-between w-full max-w-screen-xl mx-auto">
-        <h1 className="flex gap-4 items-center">
-          <Link
-            to="/"
-            className="flex items-center gap-2 text-xl font-extrabold text-primary"
-          >
-            <BiMoviePlay className="size-6" />
-            ScenePick
+        <div className="flex items-center gap-8">
+          {" "}
+          {/* 로고와 메뉴 사이 간격 조절 */}
+          <Link to="/" className="flex items-center">
+            <Logo />
           </Link>
-          <Link to="/movie" className="hover:text-primary font-bold">
-            영화
-          </Link>
-          <Link to="/drama" className="hover:text-primary font-bold">
-            드라마
-          </Link>
-          <Link to="/ost" className="hover:text-primary font-bold">
-            OST
-          </Link>
-        </h1>
+          <nav className="flex gap-6 items-center">
+            <Link
+              to="/movie"
+              className="text-gray-600 hover:text-blue-600 font-semibold transition-colors"
+            >
+              영화
+            </Link>
+            <Link
+              to="/drama"
+              className="text-gray-600 hover:text-blue-600 font-semibold transition-colors"
+            >
+              드라마
+            </Link>
+            <Link
+              to="/ost"
+              className="text-gray-600 hover:text-blue-600 font-semibold transition-colors"
+            >
+              OST
+            </Link>
+          </nav>
+        </div>
 
         <nav className="flex gap-4 items-center">
           {/*<Button asChild>*/}
