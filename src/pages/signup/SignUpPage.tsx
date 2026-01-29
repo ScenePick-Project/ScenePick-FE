f1f6c02649ffc9f3352d26f1ba3c0a8743858f36
onhann <gusgus1467@naver.com>
2026-01-29 23:21:26 +0900

    refac: 회원가입 페이지 로고 수정

--- src/pages/signup/SignUpPage.tsx
+++ src/pages/signup/SignUpPage.tsx
@@ -4,7 +4,9 @@ import ReturnHomeHeader from "@components/common/ReturnHomeHeader.tsx";
 export default function SignUpPage() {
   return (
     <div className="min-h-screen flex flex-col gap-4 justify-center items-center px-4">
-      <ReturnHomeHeader />
+      <div>
+        <ReturnHomeHeader />
+      </div>
       <div className="w-full max-w-md mx-auto">
         <UserSignupForm />
       </div>
